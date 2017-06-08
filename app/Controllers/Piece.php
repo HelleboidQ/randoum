91f0f3af376bbc5350b9aa4958057322be959573
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-06-08 21:29:03 +0200

    Dylan - A - ajout controller Piece  fonctionnement de base

---
+++ app/Controllers/Piece.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Welcome controller
+ *
+ * @author David Carr - dave@novaframework.com
+ * @version 3.0
+ */
+
+namespace App\Controllers;
+
+use Core\View;
+use Core\Controller;
+use Helpers\Session;
+
+/**
+ * Sample controller showing a construct and 2 methods and their typical usage.
+ */
+class Piece extends Controller
+{
+    /**
+     * Call the parent construct
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Define Index page title and load template files
+     */
+    public function index()
+    {
+        $data['title'] = $this->language->get('welcomeText');
+        $data['welcomeMessage'] = $this->language->get('welcomeMessage');
+
+        View::renderTemplate('header', $data);
+        View::render('Piece/piece', $data);
+        View::renderTemplate('footer', $data);
+    }
+
+}
---
+++ app/Views/Piece/Piece.php
@@ -0,0 +1,52 @@
+<?php
+$result_piece = rand(0,1);
+?>
+
+<div id="piece" >
+	<img src="app/Templates/Default/Assets/images/pile.png" id="pile" width="200"  height="200" onclick="toto();" />
+	<!--<ul class="fc-slides">
+		<li>
+		</li>
+		<li><img src="app/Templates/Default/Assets/images/face.jpg" /></li>
+	</ul>-->
+</div>
+
+
+<script>
+	/*console.log(<?= $result_piece?>);
+	var temps_piece = 10 + <?= $result_piece ?> ; */
+	function toto(){
+		temps_piece = 10 + (Math.floor(Math.random() * (1 - 0 + 1) + 0));
+		console.log(temps_piece);
+		for(var i=0; i<= temps_piece; i++){
+			rotate(i);
+		}
+		
+	}
+
+	function rotate(i){
+		setTimeout(function(){
+	//$('#'+id).animate('transform: rotate3d(0,1,0,60deg);');
+	var source = "";
+	var degree = 0;
+	if(i === temps_piece){
+		degree=0
+	}else{
+		degree = -180;
+	}
+	$('#pile').animate({  borderSpacing: degree }, {
+		step: function(now,fx) {
+			$(this).css('transform' ,'rotate3d(0,1,0,'+now+'deg)');
+		},
+		duration:300
+	},'linear');
+	if(i%2 === 0){
+		source = "app/Templates/Default/Assets/images/face.jpg";
+	}else{
+		source = "app/Templates/Default/Assets/images/pile.png";
+	}
+	$('#pile').attr('src', source);
+}, 300*i);
+	}
+
+</script>
